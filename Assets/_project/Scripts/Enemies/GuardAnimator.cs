efd049cb1b2b5fc88d748ab6064ccae65fb5b7b3
hebiii994 <salvatoreguglielmini964@gmail.com>
2025-08-12 19:29:41 +0200

    animazioni_MC_Sentinel

---
+++ Assets/_project/Scripts/Enemies/GuardAnimator.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GuardAnimator : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private NavMeshAgent _agent;
+
+    [SerializeField] private float _animationSmoothFactor = 10f;
+
+    private void Awake()
+    {
+        if (_animator == null)
+        {
+            _animator = GetComponent<Animator>();
+        }
+        if (_agent == null)
+        {
+            _agent = GetComponent<NavMeshAgent>();
+        }
+    }
+        void Update()
+        {
+            float targetSpeed = _agent.velocity.magnitude;
+            float currentAnimatorSpeed = _animator.GetFloat("Speed");
+            float smoothedSpeed = Mathf.Lerp(currentAnimatorSpeed, targetSpeed, Time.deltaTime * _animationSmoothFactor);
+            _animator.SetFloat("Speed", smoothedSpeed);
+
+        }
+    }
+
